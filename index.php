4475ee7df520bcf7ed61b08c0053f323aac2d6bc
Jorge <jorgegabriel.dev@gmail.com>
2026-04-29 21:10:49 -0300

    primeiro exercicio

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Exercícios php</title>
+</head>
+<body>
+    <?php
+     echo 'iniciando estudos em php';
+    ?>
+</body>
+</html>
