4fa8b59c274de8596b3120298f3367218a3def1d
Jorge <jorgegabriel.dev@gmail.com>
2026-04-29 21:41:14 -0300

    segunda atividade

--- index.php
+++ index.php
@@ -3,11 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
     <title>Lista de Exercícios php</title>
 </head>
 <body>
-    <?php
-     echo 'iniciando estudos em php';
-    ?>
+<div class="container">
+    <div class="item">
+        <?php
+        echo 'iniciando estudos em php';
+        ?>
+    </div>
+
+    <div class="item">
+        <?php
+            $escola = 'Senac Americana';
+
+            echo $escola;
+        ?>
+    </div>
+
+</div>
+
 </body>
 </html>
